1f2726d8b77946767ee6416a0e5d0a7f2b175aa8
Phichet Sriwichai <hs5pee@gmail.com>
2025-01-05 20:32:30 +0700

    Update app.js

--- app.js
+++ app.js
@@ -35,5 +35,5 @@ app.get('/', (req, res) => {
 
 // เริ่มต้นเซิร์ฟเวอร์
 server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on -- http://localhost:${PORT}`);
 });
